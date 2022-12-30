d8ef6b2b4d831fa10530c24e12c0948b0b1a6b6a
Adarsh Desai <desaiadarsh22@gmail.com>
2022-12-30 16:21:46 +0530

    Prevented default behaviour of links which are used as buttons

--- src/script.js
+++ src/script.js
@@ -8,7 +8,9 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+// Function to toggle the hidden class to display/hide the modal window
+const openModal = function (e) {
+  e.preventDefault(); // To prevent the page scrolling back to the top
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,12 +20,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal); // clicking the close modal button
+overlay.addEventListener('click', closeModal); //Click outside modal
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
+// Close modal when the escape key is pressed
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
